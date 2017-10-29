794549d63aeead1db20703bb4f845d9a71f2319f
alexkzm <alexkzm@gmail.com>
2017-10-29 18:26:43 -0400

    more js

--- script.js
+++ script.js
@@ -7,8 +7,39 @@ const snap = document.querySelector('.snap')
 function getVideo() {
 	navigator.getUserMedia({video: true, audio: false})
 	.then(localMeidaStream => {
-
+	video.src = window.URL.createObjectURL(localMeidaStream)
+	video.play()
+	})
+	.catch(error => {
+		console.error('camera settings', error)
 	})
 }
 
+function paintToCanvas() {
+	const width = video.videoWidth
+	const height = video.videoHeight
+	canvas.width = width
+	canvas.height = height
+
+	return setInterval(() => {
+		ctx.drawImage(video, 0, 0, width, height)
+	}, 16)
+}
+
+function takePhoto() {
+	snap.currentTime = 0
+	snap.play()
+
+	const data = canvas.toDataURL('image/jpeg')
+	const link = document.createElement('a')
+	link.href = data
+	link.setAttribute('download', 'fun')
+	link.innerHTML = '<img src="${data}"" alt="funman" />'
+	strip.insertBefore(link, strip.firstChild)
+}
+
 getVideo()
+
+video.addEventListener('canplay', paintToCanvas)
+
+
